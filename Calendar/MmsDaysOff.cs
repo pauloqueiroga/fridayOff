f2a265e16b0640ccae3a9a5de2453b0eeee77a8c
Paulo Queiroga <githubpublic@pauloqueiroga.com>
2018-03-11 12:57:19 -0700

    First prototype of MMS Days Off

---
+++ Calendar/MmsDaysOff.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queiroga.FridayOff.Calendar
+{
+    public class MmsDaysOff : DateProvider
+    {
+        private const string FridayOffDescription = "Friday Off";
+        private static readonly Dictionary<DateTime, string> _mmsDaysOff = new Dictionary<DateTime, string>
+        {
+            { new DateTime(2018, 1, 1), "New Year's Day" },
+            { new DateTime(2018, 3, 30), "Good Friday" },
+            { new DateTime(2018, 5, 28), "Memorial Day" },
+            { new DateTime(2018, 7, 4), "Independence Day" },
+            { new DateTime(2018, 9, 3), "Labor Day" },
+            { new DateTime(2018, 11, 22), "Thanksgiving Thursday" },
+            { new DateTime(2018, 12, 24), "Christmas' Eve" },
+            { new DateTime(2018, 12, 25), "Chirstmas Day" },
+            { new DateTime(2018, 12, 26), "Forced Paid Time Off" },
+            { new DateTime(2018, 12, 27), "Forced Paid Time Off" },
+            { new DateTime(2018, 12, 31), "Forced Paid Time Off" },
+        };
+
+        public MmsDaysOff() : base(_mmsDaysOff, 5000)
+        {
+            var fridayOff = new DateTime(2018, 1, 12);
+            var finalDay = new DateTime(2018, 12, 31);
+
+            while (fridayOff < finalDay)
+            {
+                CreateDayOff(fridayOff, FridayOffDescription);
+                fridayOff = fridayOff.AddDays(14);
+            }
+        }
+    }
+}
---
+++ CalendarTest/MmsDaysOffTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Queiroga.FridayOff.Calendar.Test
+{
+    [TestClass]
+    public class MmsDaysOffTests
+    {
+        [TestMethod]
+        public void Get2018DaysOffShouldReturn()
+        {
+            const int expectedDaysOff = 143;
+            var january1 = new DateTime(2018, 1, 1);
+            var target = new MmsDaysOff();
+
+            var daysOff2018 = target.GetNextDaysOff(january1, expectedDaysOff);
+
+            Assert.IsNotNull(daysOff2018);
+            var flattenedDaysOff = daysOff2018.ToArray();
+            CollectionAssert.AllItemsAreNotNull(flattenedDaysOff);
+            CollectionAssert.AllItemsAreUnique(flattenedDaysOff);
+            Assert.AreEqual(expectedDaysOff, flattenedDaysOff.Length);
+            CollectionAssert.Contains(flattenedDaysOff, new DateTime(2018, 12, 31));
+        }
+    }
+}
